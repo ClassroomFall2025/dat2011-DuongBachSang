aee57023c12bda8bad0edf1d4d86fdacef102b83
DuongBachSang <duongsang.28102006@gmail.com>
2025-10-11 07:19:19 +0700

    em nộp lab 5 ạ

---
+++ sinhvienpoly.py
@@ -0,0 +1,26 @@
+class SinhVienPoLy:
+    def __init__(self):
+        pass
+    
+    def get_diem(self):
+        pass
+    
+    def get_hoc_luc(self):
+        pass
+
+    def xuat(self):
+        pass
+
+    class SinhVienIT(SinhVienPoly):
+        def __init__(self, ten_sv, nganh, java, html, css):
+            pass
+
+        def get_diem(self):
+            pass
+
+    class SinhVienBiz(SinhVienPoLy):
+        def __init__(self, ten_sv, nganh, marketing, sales):
+            pass
+
+        def get_diem(self):
+            pass
