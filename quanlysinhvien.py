143b536380abbe517c8b77d2e73437131f68a79b
DuongBachSang <duongsang.28102006@gmail.com>
2025-10-14 20:41:57 +0700

    em nộp lab6 ạ

--- quanlysinhvien.py
+++ quanlysinhvien.py
@@ -1,17 +1,46 @@
 import sinhvienpoly as svpl
 
 class QuanLySinhVien:
-    def __init__(self, ):
+    def __init__(self):
         self.dssv = []
 
     def nhap_dssv(self):
-        pass 
+        while True:
+            ten = input("nhập tên sinh viên: ")
+            nganh = input("nhập ngành sinh viên học: ")
+
+            if nganh.lower() == "it":
+                java = float(input("nhập điểm Java: "))
+                html = float(input("nhập điểm HTML: "))
+                css = float(input("nhập điểm CSS: "))
+                sv = svpl.SinhVienIT(ten, nganh, java, html, css )
+                self.dssv.append(sv)
+            elif nganh.lower() == "biz":
+                marketing = float(input("nhập điểm marketing: "))
+                sales = float(input("nhập điểm Sales: "))
+                sv = svpl.SinhVienBiz(ten, nganh, marketing, sales)
+                self.dssv.append(sv)
+            elif nganh.lower() == "esc":
+                break
+            else:
+                print("Ngành không hợp lệ vui lòng nhập lại")
+        return self.dssv
+    def xuat_dssv(self): 
+        if not self.dssv:
+            print("danh sách sinh viên trống")
+        else:
+            print(f'{"tên sinh viên"}, {"ngành"}, {"diem"}, {"học lực"}')
+            for sv in self.dssv:
+                sv.xuat()
+    def xuat_dssv_gioi(self): 
+        print("==Danh sách sinh viên giỏi==")
+        for sv in self.dssv:
+            if sv.get_hoc_luc() in ["Giỏi", "Xuất sắc"]:
+                sv.xuat()
+        print("---------------------------------")
  
-    def xuat_ddsv(self): 
-        pass
- 
-    def xuat_ddsv_gioi(self): 
-        pass
- 
-    def xuat_ddsv_diem(self):
-        pass
+    def xuat_dssv_diem(self):
+        print("==Sắp xếp sinh viên theo điểm==")
+        self.dssv.sort(key =lambda sv: sv.get_diem(), reverse=True)
+        for sv in self.dssv:
+            sv.xuat()
--- sinhvienpoly.py
+++ sinhvienpoly.py
@@ -1,26 +1,59 @@
 class SinhVienPoLy:
-    def __init__(self):
-        pass
+    def __init__(self, ten_sv, nganh):
+        self.__ten_sv = ten_sv
+        self.__nganh = nganh
+
+    def get_ten_sv(self):
+        return self.__ten_sv
+    
+    def set_ten_sv(self, ten_sv):
+        self.__ten_sv = ten_sv
+
+    def get_nganh(self):
+        return self.__nganh
+    
+    def set_nganh(self, nganh):
+        self.__nganh = nganh
+
+
     
     def get_diem(self):
         pass
     
     def get_hoc_luc(self):
-        pass
-
+        diem = self.get_diem()
+        if diem < 5:
+            return "Yếu"
+        elif 5 <= diem < 7:
+            return "Trung bình"
+        elif 7 <= diem < 8:
+            return "Khá"
+        elif 8 <= diem < 9:
+            return "Giỏi"
+        else:
+            return "Xuất sắc"
     def xuat(self):
-        pass
+        print(f"tên sinh viên: {self.__ten_sv}")
+        print(f"ngành học: {self.__nganh}")
+        print(f"điểm trung bình: {self.get_diem()}")
+        print(f"học lực: {self.get_hoc_luc()}")
 
-    class SinhVienIT(SinhVienPoly):
-        def __init__(self, ten_sv, nganh, java, html, css):
-            pass
 
-        def get_diem(self):
-            pass
+class SinhVienIT(SinhVienPoLy):
+    def __init__(self, ten_sv, nganh, java, html, css):
+        super().__init__(ten_sv, nganh)
+        self.java = java
+        self.html = html
+        self.css = css
 
-    class SinhVienBiz(SinhVienPoLy):
-        def __init__(self, ten_sv, nganh, marketing, sales):
-            pass
+    def get_diem(self):
+        return (2 * self.java + self.html + self.css) / 4 
 
-        def get_diem(self):
-            pass
+class SinhVienBiz(SinhVienPoLy):
+    def __init__(self, ten_sv, nganh, marketing, sales):
+        super().__init__(ten_sv, nganh)
+        self.marketing = marketing
+        self.sales = sales
+
+    def get_diem(self):
+        return (2 * self.marketing + self.sales) / 3
